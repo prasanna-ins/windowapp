866a85930c3bdf75f7e08b7f4573dd94cb0977e5
prasanna-ins <76608284+prasanna-ins@users.noreply.github.com>
2021-09-03 20:08:30 +0530

    Update practice on class.py

--- practice on class.py
+++ practice on class.py
@@ -1,7 +1,3 @@
 class circle():
-    pi=3.14
-    print("[ennter the radius as per u r need")
-    radius=int(input())
-    #def __init__(self,radius=int(input())):
-     #   self.radius= radius
+  
   
