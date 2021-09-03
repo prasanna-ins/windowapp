56af2c0f7022c5018cbcc2723921a61f225c646e
prasanna-ins <naikprasanna692@gmail.com>
2021-09-03 20:04:59 +0530

    Update practice on class.py

--- practice on class.py
+++ practice on class.py
@@ -4,11 +4,4 @@ class circle():
     radius=int(input())
     #def __init__(self,radius=int(input())):
      #   self.radius= radius
-    def circuference(self):
-        return 2 * circle.pi * self.radius
-    def area(self):
-        # this is a area function which is used to find the area of the circle
-        return circle.pi * self.radius * self.radius
-#output=circle()
-print(circle().circuference())
-print(circle().area())
+  
